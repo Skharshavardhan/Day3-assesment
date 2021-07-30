a4e8fef95df1383f3e49169cd55224b21d65a0c9
cyber sk <64033357+Skharshavardhan@users.noreply.github.com>
2021-07-30 13:21:46 +0530

    day3 assignment submitted
    day3 assignment submitted

---
+++ test.py
@@ -0,0 +1,21 @@
+print("welcome to the gift giving event")
+amt = int(input('Enter the gifting person number: '))
+if amt >= 1:
+  print("1 has recived gift from 2")
+elif amt >= 2:
+  print('2 has recived gift from 1')
+
+if amt >= 3:
+  print("3 has recived gift from 4")
+elif amt >= 4:
+  print('4 has recived gift from 5')
+
+if amt >= 5:
+  print("5 has recived gift from 3")
+
+
+  
+
+
+
+
